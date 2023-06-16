527a4edb691504163004e2c1190230da32c0d4ae
Joshua <twogoodgamer2@gmail.com>
2023-06-16 17:30:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ cd ~/.pikdum
 wget -O steam-deck.zip https://github.com/SirStig/Steam-deck-tests/archive/refs/heads/main.zip
 unzip -o steam-deck.zip
 rm steam-deck.zip
-mv steam-deck-tests-main steam-deck-master
+mv Steam-deck-tests-main steam-deck-master
 
 ~/.pikdum/steam-deck-master/post-install.sh
 
