f456ce8ec513e947cd3420a23c836548cffb3958
Joshua <twogoodgamer2@gmail.com>
2023-06-16 17:15:47 -0600

    Test: Changed URL's to run from this git

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-VERSION=$(curl https://raw.githubusercontent.com/pikdum/steam-deck/main/version.txt)
+VERSION=$(curl https://raw.githubusercontent.com/SirStig/Steam-deck-tests/main/version.txt)
 
 printf "Installing Pikdum's Tools Version: $VERSION";
 
@@ -9,11 +9,13 @@ mkdir -p ~/.local/share/applications/
 
 mkdir -p ~/.pikdum
 cd ~/.pikdum
-wget -O steam-deck.zip https://github.com/pikdum/steam-deck/archive/refs/heads/master.zip
+wget -O steam-deck.zip https://github.com/SirStig/Steam-deck-tests/archive/refs/heads/main.zip
 unzip -o steam-deck.zip
 rm steam-deck.zip
+mv steam-deck-tests-main steam-deck-master
 
 ~/.pikdum/steam-deck-master/post-install.sh
 
 printf "Success! Exiting in 3..."
 sleep 3
+
