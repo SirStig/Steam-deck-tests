7fd7d7105156b6219cd1689bd87f4453b248a65c
Joshua <twogoodgamer2@gmail.com>
2023-06-16 19:20:45 -0600

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-printf "Checking for shortcuts...";
+printf "INFO: Checking for shortcuts...";
 
 ln -sf ~/.pikdum/steam-deck-master/update.desktop ~/Desktop/pikdum-update.desktop
 
@@ -9,24 +9,25 @@ if [ -f "~/.local/share/applications/vortex.desktop" ] && [ ! -f "~/.vortex-linu
    rm -f ~/.local/share/applications/vortex.desktop
    rm -f ~/Desktop/vortex.desktop
    ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop || true
-   printf "Vortex not found, adding install-vortex.desktop to desktop.";
+   printf "FIX: Vortex not found, adding install-vortex.desktop to desktop.";
 else if [ ! -f "~/.local/share/applications/vortex.desktop" ] && [ -f "~/.vortex-linux/compatdata/pfx/drive_c/Program Files/Black Tree Gaming Ltd/Vortex/Vortex.exe" ]; then
-    ln -sf ~/.local/share/applications/vortex.desktop ~/Desktop
-    printf "Vortex shortcut not found, creating new one.";
+    ln -sf ~/.local/share/applications/vortex.desktop ~/Desktop/
+    printf "FIX: Vortex shortcut not found, creating new one.";
 else
    ln -sf ~/.pikdum/steam-deck-master/vortex/vortex-post-updater.desktop ~/Desktop/
    ln -sf ~/.pikdum/steam-deck-master/vortex/update-vortex-library.desktop ~/Desktop/
+   printf "SUCCESS: Found all shrotcuts!";
 fi
 
-printf "Checking for old Pikdum's files..."
+printf "INFO: Checking for old Pikdum's files...";
 #Check if post-deploy scripts and shortcuts exist.
 if [ -L "~/Desktop/fallout3-post-deploy.desktop" ] || [ -L "~/Desktop/fallout4-post-deploy.desktop" ] || [ -L "~/Desktop/fallout4-post-deploy.desktop" ] || [ -L "~/Desktop/falloutnv-post-deploy.desktop" ] || [ -L "~/Desktop/falloutnv-pre-deploy.desktop" ] || [ -L "~/Desktop/oblivion-post-deploy.desktop" ] || [ -L "~/Desktop/skyrim-post-deploy.desktop" ] || [ -L "~/Desktop/skyrimle-post-deploy.desktop" ]; then
    rm "~/Desktop/fallout4-post-deploy.desktop" "~/Desktop/fallout3-post-deploy.desktop" "~/Desktop/falloutnv-post-deploy.desktop" "~/Desktop/falloutnv-pre-deploy.desktop" "~/Desktop/oblivion-post-deploy.desktop" "~/Desktop/skyrim-post-deploy.desktop" "~/Desktop/skyrimle-post-deploy.desktop";
-   printf "Found old Deploy links removing..."
+   printf "FIX: Found old Deploy links removing...";
 fi
 
 mkdir -p /run/media/mmcblk0p1/vortex-downloads || true
 
-printf "Done! Sleeping in 3...";
+printf "END: Done! Sleeping in 3...";
 
 sleep 20
