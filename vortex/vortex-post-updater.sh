b5c9ff02cbcd91a5d8d4be7b307fd389b543606f
Joshua <twogoodgamer2@gmail.com>
2023-06-16 18:15:54 -0600

    Init: Initial setup of script extender setup
    Additions:
    
    - Created a vortex-post-updater.sh/desktop/ico to automatically find the Steam Library, search for script extender launchers and rename them to the games main launcher for steam deck.

---
+++ vortex/vortex-post-updater.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+printf "%s\n" "INFO: Searching for all script extender launchers!";
+rmlink(){
+    if [ -h "$1" ];
+    then unlink "$1";
+    fi;
+}
+mklink(){
+    rmlink "$2";
+    ln -s -T "$1" "$2";
+}
+manifest_attribute(){
+    grep -a -o -e "\"$2\"[[:space:]]*\".*\"$" "$1" \
+    | sed "s/\"$2\"[[:space:]]*\"//;s/\"$//";
+};
+manifest_userconfig_attribute(){
+    manifest_attribute \
+    <(grep \
+        -a -o -P -z \
+        "\"UserConfig\"\s*{(\s*[^}]*\s*)*}" \
+        "$1"\
+    ) "$2";
+};
+manifest_mountedconfig_attribute(){
+    manifest_attribute \
+    <(grep \
+        -a -o -P -z \
+        "\"MountedConfig\"\s*{(\s*[^}]*\s*)*}" \
+        "$1"\
+    ) "$2";
+};
+sed "s/\"\/.*\"$/\"C:\\\\\\\\Program Files \(x86\)\\\\\\\\Steam\"/g" < \
+~/.steam/steam/steamapps/libraryfolders.vdf |& tee \
+"$VORTEX_PREFIX/drive_c/Program Files (x86)/Steam/steamapps/libraryfolders.vdf" \
+"$VORTEX_PREFIX/drive_c/Program Files (x86)/Steam/config/libraryfolders.vdf" \
+1>/dev/null;
+STEAM_LIBRARY_PATHS=();
+while read -r library; do
+    rmdir --ignore-fail-on-non-empty "$library"/steamapps/common/* 2>/dev/null;
+    if [ -d "$library" ] && [ -d "$library/steamapps/common" ]; then
+        if [ -d "$library/steamapps/compatdata/" ]; then
+        STEAM_COMPATDATA=("$(find "$library/steamapps/compatdata/" -type d -name "pfx")");
+        for compatdata in "${STEAM_COMPATDATA[@]}"; do
+            rmdir --ignore-fail-on-non-empty "$compatdata" 2>/dev/null;
+        done;
+        fi;
+        STEAM_LIBRARY_PATHS+=("$library");
+    fi;
+done < <(grep -a -o -e "/[^\"]*" ~/.steam/steam/steamapps/libraryfolders.vdf);
+printf "%s\n" "INFO: Found Steam Library at \"$library\"!";
+
+
+
+printf "%s\n" "DONE: Finished!";
+
+printf "Going to sleep in 3...";
+
+sleep 3
